3bb57d789d600bd8dde82d275fabd0f5758cd1e3
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-02-10 14:14:11 -0300

    docs: atualização layout

--- meu_curriculo/src/pages/index.js
+++ meu_curriculo/src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Currículo Lídia`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
